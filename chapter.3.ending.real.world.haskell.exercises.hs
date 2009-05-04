8c5e35f6632b581d77dab18afd4729d06d76c85a
Charlie Flowers <cflowers@mindspring.com>
2009-05-04 02:22:13 -0400

    Complated the test case spec (not passing or even implemented yet though)

--- chapter.3.ending.real.world.haskell.exercises.hs
+++ chapter.3.ending.real.world.haskell.exercises.hs
@@ -307,8 +307,10 @@ data TestCase_ListOfPoints = TestCase_ListOfPoints {
 }
 
 testCase_ListOfPoints = 
-	TestCase_ListOfPoints [(Point 1 1), (Point (-4) 2), (Point (-5) 4), (Point (-5) (-2)), 
-		(Point (-3) (-3))] [RightTurn, LeftTurn, LeftTurn, LeftTurn, RightTurn, RightTurn, RightTurn, Straight, Straight, LeftTurn, Straight, RightTurn]
+    TestCase_ListOfPoints [(Point 1 1), (Point (-4) 2), (Point (-5) 4), (Point (-5) (-2)), 
+        (Point (-3) (-3)), (Point (-2) 3), (Point 5 1), (Point 1 (-3)), (Point 1 (-2)), (Point 1 (-1)), (Point 1 (-2)),
+        (Point 5 (-2)), (Point 3 (-2)), (Point 3 4)] 
+        [RightTurn, LeftTurn, LeftTurn, LeftTurn, RightTurn, RightTurn, RightTurn, Straight, Straight, LeftTurn, Straight, RightTurn]
 
 
 
